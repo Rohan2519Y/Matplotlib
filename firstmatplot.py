7056a03aaece07f758df0b61c104485162e3d605
Rohan2519Y <rohankumar17645@gmail.com>
2026-08-02 11:08:02 +0530

    Creating the first matplotlib

--- firstmatplot.py
+++ firstmatplot.py
@@ -2,8 +2,13 @@ import matplotlib.pyplot as plt
 
 X = ['A', 'B', 'C', 'D', 'E']
 Y = [100, 200, 300, 400, 500]
+Z = [200, 300, 400, 500, 600]
 # plt.bar(X, Y, color='red')
-plt.bar(X, Y, color=['pink', 'green', 'blue', 'red', 'yellow'], width=0.7)
+# plt.bar(X, Y, color=['pink', 'green', 'blue', 'red', 'yellow'], width=0.5, align='center', edgecolor='black', linewidth=2, linestyle="-.", alpha=0.8)
+
+plt.bar(X, Z, color='green')
+plt.bar(X, Y, color='red')
+
 plt.xlabel("X", fontsize=20)
 plt.ylabel("Y")
 plt.title("X annnnnd Y")
