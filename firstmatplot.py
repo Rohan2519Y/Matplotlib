abd1aee516db883019a3784806c265e582add39d
Rohan2519Y <rohankumar17645@gmail.com>
2026-08-02 10:46:33 +0530

    first commit

--- firstmatplot.py
+++ firstmatplot.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 
-X = ['A', 'B', 'C', 'D']
-Y = [100, 200, 300, 400]
-plt.bar(X, Y)
+X = ['A', 'B', 'C', 'D', 'E']
+Y = [100, 200, 300, 400, 500]
+# plt.bar(X, Y, color='red')
+plt.bar(X, Y, color=['pink', 'green', 'blue', 'red', 'yellow'])
+plt.xlabel("X", fontsize=20)
+plt.ylabel("Y")
+plt.title("X annnnnd Y")
+# plt.pie( Y)
 plt.show()
