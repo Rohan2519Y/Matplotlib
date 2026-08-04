b1bcc94d7cfac9fea0c660be9bc35395ee9194f0
Rohan2519Y <rohankumar17645@gmail.com>
2026-08-04 18:59:06 +0530

    Adding more Functions

--- firstmatplot.py
+++ firstmatplot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 X = ['A', 'B', 'C', 'D', 'E']
 Y = [100, 200, 300, 400, 500]
@@ -6,11 +7,24 @@ Z = [200, 300, 400, 500, 600]
 # plt.bar(X, Y, color='red')
 # plt.bar(X, Y, color=['pink', 'green', 'blue', 'red', 'yellow'], width=0.5, align='center', edgecolor='black', linewidth=2, linestyle="-.", alpha=0.8)
 
-plt.bar(X, Z, color='green')
-plt.bar(X, Y, color='red')
+# plt.bar(X, Z, color='green', label='Z')
+# plt.bar(X, Y, color='red', label='Y')
 
-plt.xlabel("X", fontsize=20)
-plt.ylabel("Y")
-plt.title("X annnnnd Y")
+# plt.xlabel("X", fontsize=20)
+# plt.ylabel("Y")
+# plt.title("X annnnnd Y")
 # plt.pie( Y)
+
+# width = 0.3
+# p = np.arange(len(X))
+# p1 = [i + width for i in p]
+
+# plt.bar(p, Z, width=width, color='green', label='Z')
+# plt.bar(p1, Y, width=width, color='red', label='Y')
+# plt.xticks(p + width/2.0, X)
+# plt.legend()
+
+# plt.scatter(X, Y, c=[10, 20, 30, 40, 50], s=50, marker='*', cmap='viridis')
+
+plt.pie(Y, labels=X, autopct='%.2f%%', explode=[0.4, 0.4, 0, 0, 0], shadow=False, radius=1.3, startangle=30, wedgeprops={"edgecolor":'black'})
 plt.show()
